7697d6c2d0ab47fa924a155586c2efb93343a484
suganthi08 <70209465+suganthi08@users.noreply.github.com>
2021-02-03 16:35:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Ahmed
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,51 @@
+var ball;
+var database;
+var pos;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var readRef = database.ref("car/position")
+    readRef.on ("value", readData )
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+
+    var writeRef = database.ref("car/position");
+    writeRef.set({
+
+     x: ball.x + x,
+     y: ball.y + y
+
+    })
+
+    
+}
+
+function readData(data){
+pos = data.val();
+ball.x = pos.x;
+ball.y = pos.y;
+
+}
